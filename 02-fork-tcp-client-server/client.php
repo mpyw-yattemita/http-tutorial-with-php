4a8f1c15b22b80585cb9046c7824acbfebceb942
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-03-13 19:52:51 +0900

    Fixed

--- 02-fork-tcp-client-server/client.php
+++ 02-fork-tcp-client-server/client.php
@@ -1,16 +1,16 @@
 <?php
 
 // TCPクライアントソケットを生成
-$fp = @stream_socket_client('tcp://localhost:8080');
+$con = @stream_socket_client('tcp://localhost:8080');
 if (!$con) {
     fwrite(STDERR, error_get_last()['message'] . "\n");
     exit(1);
 }
 
 // 終端まで読み出す
-while (false !== $line = fgets($fp)) {
+while (false !== $line = fgets($con)) {
     echo "Received: $line";
 }
 
 // TCPコネクションを切断する
-fclose($fp);
+fclose($con);
